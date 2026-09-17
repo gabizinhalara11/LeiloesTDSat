0f1956762cd9e6235e78ed41c206e89e1cb1e48b
Gabriela <gabe35884@gmail.com>
2026-09-17 17:34:59 -0300

    Implementa funcionalidade de salvar produto com mensagem de sucesso/erro

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,8 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
+
 
 
 public class ProdutosDAO {
@@ -24,13 +26,46 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
+        conn = new conectaDAO().connectDB();
         
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.execute();
+
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
+        
+        listagem = new ArrayList<>();
+        conn = new conectaDAO().connectDB();
+        
+        try {
+            String sql = "SELECT * FROM produtos";
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()){
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
+        }
         
         return listagem;
     }
